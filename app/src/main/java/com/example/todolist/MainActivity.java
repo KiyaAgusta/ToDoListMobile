a5a1e4d8d48b91d443fe287093ac159592f03d17
I Made Akira Ivandio Agusta <ivandioakira@gmail.com>
2020-09-17 21:19:19 +0800

    First commit

---
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] toDo = {"Nugas", "Makan", "Mandi", "Rebahan", "Olahraga"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter listAdapter = new ArrayAdapter<String>(this, R.layout.activity_list, toDo);
+        ListView listView = (ListView) findViewById(R.id.itemListView);
+        listView.setAdapter(listAdapter);
+    }
+}
